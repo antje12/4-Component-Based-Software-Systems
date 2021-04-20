119a348fc0852930c8f42491695c03fa2b5ef8f3
anton <antontj@live.dk>
2021-04-20 16:48:34 +0200

    mockito-core update

--- v8 - AsteroidsTest/OSGiPlayer/src/test/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerPluginTest.java
+++ v8 - AsteroidsTest/OSGiPlayer/src/test/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerPluginTest.java
@@ -10,22 +10,22 @@ import static org.mockito.Mockito.*;
 import static org.mockito.ArgumentMatchers.any;
 
 public class PlayerPluginTest {
-    
+
     public PlayerPluginTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -36,20 +36,20 @@ public class PlayerPluginTest {
     @Test
     public void testStart() { // using mock
         System.out.println("start");
-        
+
         GameData gameData = mock(GameData.class);
         when(gameData.getDisplayWidth()).thenReturn(800);
         when(gameData.getDisplayHeight()).thenReturn(600);
-        
+
         World world = mock(World.class);
         when(world.addEntity(any(Entity.class))).thenReturn("1");
-        
+
         PlayerPlugin instance = new PlayerPlugin();
         instance.start(gameData, world);
 
         verify(gameData).getDisplayWidth();
         verify(gameData).getDisplayHeight();
-        
+
         verify(world).addEntity(any(Entity.class));
     }
 
@@ -58,19 +58,19 @@ public class PlayerPluginTest {
      */
     @Test
     public void testStop() { // using mock
-        System.out.println("stop");        
-        
+        System.out.println("stop");
+
         List<Entity> testPlayers = new ArrayList<Entity>();
         testPlayers.add(new Player());
-        
-        GameData gameData = mock(GameData.class); 
-        
+
+        GameData gameData = mock(GameData.class);
+
         World world = mock(World.class);
         when(world.getEntities(Player.class)).thenReturn(testPlayers);
-        
+
         PlayerPlugin instance = new PlayerPlugin();
         instance.stop(gameData, world);
-        
+
         verify(world).getEntities(Player.class);
         verify(world).removeEntity(any(Entity.class));
     }
--- v8 - AsteroidsTest/OSGiPlayer/src/test/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerSystemTest.java
+++ v8 - AsteroidsTest/OSGiPlayer/src/test/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerSystemTest.java
@@ -13,22 +13,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerSystemTest {
-    
+
     public PlayerSystemTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -39,30 +39,30 @@ public class PlayerSystemTest {
     @Test
     public void testProcess() { // not using mock
         System.out.println("process");
-        
+
         GameData gameData = new GameData();
         gameData.getKeys().setKey(GameKeys.UP, true);
         gameData.setDelta(1);
         gameData.setDisplayWidth(800);
         gameData.setDisplayHeight(600);
-        
+
         World world = new World();
         PlayerPlugin playerPlugin = new PlayerPlugin();
         playerPlugin.start(gameData, world);
-        
+
         Entity player = world.getEntities(Player.class).get(0);
         PositionPart pos = player.getPart(PositionPart.class);
-        
+
         float startX = pos.getX();
         float startY = pos.getY();
-        
+
         PlayerSystem instance = new PlayerSystem();
         instance.process(gameData, world);
-        
+
         float endX = pos.getX();
         float endY = pos.getY();
-        
-        assertEquals((int)startX, (int)endX);
-        assertNotEquals((int)startY, (int)endY);
+
+        assertEquals((int) startX, (int) endX);
+        assertNotEquals((int) startY, (int) endY);
     }
 }
